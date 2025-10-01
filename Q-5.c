55e612dd9cd9c3a051a5b82de6a874ac3a970963
ATUL PAL <palatul8141@gmail.com>
2025-10-01 10:00:26 +0530

    Add files via upload

---
+++ Q-5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main (){
+    int num = 11;
+    for(int i=0; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            // printf("*");
+            printf("%d ",num);
+            num++;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
